a1eb403a17d8071b91c09073efa9fe5a2ecc8f9d
gimn70009 <risa1110@hansung.ac.kr>
2025-07-19 21:27:48 +0900

    feat: JWT 2. 프로젝트 생성 및 JWT 의존성 추가

---
+++ src/main/java/com/example/springjwt/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.example.springjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller// 스프링에 컨트롤러로 등록해서 관리해야 함
+@ResponseBody // 웹 페이지를 리턴해주는 것이 아닌, 특정한 객체 데이터나 스트링 데이터를 응답해줄 것이기 때문
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminP() {
+
+        return "admin Controller";
+    }
+}
