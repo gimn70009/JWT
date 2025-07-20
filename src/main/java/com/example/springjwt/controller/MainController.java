a5717510bea81fd7ea2ff53925b3daf8871e2943
gimn70009 <risa1110@hansung.ac.kr>
2025-07-20 20:39:07 +0900

    feat: JWT 12. 세션 정보

--- src/main/java/com/example/springjwt/controller/MainController.java
+++ src/main/java/com/example/springjwt/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.example.springjwt.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller// 스프링에 컨트롤러로 등록해서 관리해야 하므로
 @ResponseBody // 웹 페이지를 리턴해주는 것이 아닌, 특정한 객체 데이터나 스트링 데이터를 응답해줄 것이기 때문
@@ -11,6 +17,15 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "Main Controller";
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller" + username + role;
     }
 }
